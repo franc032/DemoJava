1beaa19d15835f73ffd90bbd2ee8ab7d59a7b349
franc032 <franco.ilich@dxc.com>
2019-02-14 10:24:31 -0300

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		helloWorld();	
+	}
+
+	public static String helloWorld()
+	{
+		String res = "Hello World";
+
+		return res;
+	}	
+}
